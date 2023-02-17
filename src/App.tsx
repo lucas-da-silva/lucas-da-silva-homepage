f21ac80a1763b0591172e23470d7d34142079d4b
lucasdasilva <xBigodinho@outlook.com>
2023-02-17 09:57:32 -0300

    build: init project typescript react

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <Routes>
+      <Nav />
+      <Route />
+    </Routes>
+  )
+}
+
+export default App;
