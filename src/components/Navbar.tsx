5fa1daa137a39b1546ab9f74e2ef78dccb63e0f8
lucasdasilva <xBigodinho@outlook.com>
2023-02-20 09:51:43 -0300

    feat: finished style for navbar links and refactore links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,43 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+
+const links = [
+  {
+    path: '/',
+    text: 'Início',
+  },
+  {
+    path: '/projects',
+    text: 'Projetos',
+  },
+  {
+    path: '/contact',
+    text: 'Contato',
+  },
+];
 
 function Navbar() {
   return (
     <nav className="flex content-center items-center justify-between">
       <div className="flex items-center">
-        <img 
-          src="/public/photo.jpg" 
-          alt="Lucas da Silva" 
+        <img
+          src="/public/photo.jpg"
+          alt="Lucas da Silva"
           className="w-10 h-15 rounded-full mr-2"
         />
         <p>Lucas da Silva</p>
       </div>
-      <div className="flex">
-        <Link className="px-3 py-2 rounded-md text-sm font-medium" to="/">Início</Link>
-        <Link to="/projects">Projetos</Link>
-        <Link to="/contact">Contato</Link>
+      <div className="flex mr-3">
+        {links.map(({ path, text }) => (
+          <Link
+            className="px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-800"
+            to={path}
+          >
+            {text}
+          </Link>
+        ))}
       </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar;
