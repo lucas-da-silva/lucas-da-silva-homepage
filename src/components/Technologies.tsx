5b29c8a48009462c0a9b85f4374ea976d8a4b731
lucasdasilva <xBigodinho@outlook.com>
2023-02-21 14:24:18 -0300

    refactor: create array Front End and Back End technologies

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,125 +1,119 @@
 import { Link } from 'react-router-dom';
 
+const technologiesFrontEnd = [
+  {
+    ref: 'https://www.javascript.com/',
+    path: '/javascript.svg',
+    text: 'JavaScript',
+  },
+  {
+    ref: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+    path: '/css-3.svg',
+    text: 'CSS 3',
+  },
+  {
+    ref: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
+    path: '/html.svg',
+    text: 'HTML 5',
+  },
+  {
+    ref: 'https://reactjs.org/',
+    path: '/react.svg',
+    text: 'React.js',
+  },
+  {
+    ref: 'https://redux.js.org/',
+    path: '/redux.svg',
+    text: 'Redux',
+  },
+  {
+    ref: 'https://jestjs.io/',
+    path: '/jest.svg',
+    text: 'Jest',
+  },
+  {
+    ref: 'https://tailwindcss.com/',
+    path: '/tailwindcss.svg',
+    text: 'Tailwindcss',
+  },
+  {
+    ref: 'https://getbootstrap.com/',
+    path: '/bootstrap-5.svg',
+    text: 'Bootstrap 5',
+  },
+];
+
+const technologiesBackEnd = [
+  {
+    ref: 'https://nodejs.org/en/',
+    path: '/nodejs.svg',
+    text: 'Nodejs',
+  },
+  {
+    ref: 'https://expressjs.com/',
+    path: '/expressjs.svg',
+    text: 'Express.js',
+  },
+  {
+    ref: 'https://www.mysql.com/',
+    path: '/mysql.svg',
+    text: 'MySQL',
+  },
+  {
+    ref: 'https://sequelize.org/',
+    path: '/sequelizejs.svg',
+    text: 'Sequelize',
+  },
+  {
+    ref: 'https://www.typescriptlang.org/',
+    path: '/typescript.svg',
+    text: 'TypeScript',
+  },
+  {
+    ref: 'https://www.mongodb.com/',
+    path: '/mongodb.svg',
+    text: 'MongoDB',
+  },
+  {
+    ref: 'https://mongoosejs.com/',
+    path: '/mongoose.svg',
+    text: 'Mongoose',
+  },
+];
+
 function Technologies() {
   return (
     <div className="flex flex-col rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40 mt-10">
       <h3 className="text-xl font-bold text-zinc-800 dark:text-zinc-100 mb-5">
         Tecnologias
       </h3>
-      <div className="flex justify-between mb-6 space-x-10">
-        <div className="w-1/2 rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40 text-zinc-900 text-sm font-semibold dark:text-zinc-100">
+      <div className="sm:flex justify-between mb-6 sm:space-x-10">
+        <div className="mb-6 sm:w-1/2 sm:mb-0 rounded-2xl border border-zinc-100 p-4 sm:p-5 dark:border-zinc-700/40 text-zinc-900 text-sm font-semibold dark:text-zinc-100">
           <h4>Front End</h4>
           <div className="flex flex-wrap justify-center mt-4 space-x-2">
-            <a href="https://www.javascript.com/">
-              <img
-                src="/technologies-logo/javascript.svg"
-                alt="JavaScript"
-                className="w-10 h-10"
-              />
-            </a>
-            <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">
-              <img
-                src="/technologies-logo/css-3.svg"
-                alt="CSS 3"
-                className="w-10 h-10"
-              />
-            </a>
-            <a href="https://developer.mozilla.org/en-US/docs/Web/HTML">
-              <img
-                src="/technologies-logo/html.svg"
-                alt="HTML 5"
-                className="w-10 h-10"
-              />
-            </a>
-            <a href="https://reactjs.org/">
-              <img
-                src="/technologies-logo/react.svg"
-                alt="React"
-                className="w-10 h-10"
-              />
-            </a>
-            <a href="https://redux.js.org/">
-              <img
-                src="/technologies-logo/redux.svg"
-                alt="Redux"
-                className="w-10 h-10"
-              />
-            </a>
-            <a href="https://jestjs.io/">
-              <img
-                src="/technologies-logo/jest.svg"
-                alt="Jest"
-                className="w-10 h-10"
-              />
-            </a>
-            <a href="https://tailwindcss.com/">
-              <img
-                src="/technologies-logo/tailwindcss.svg"
-                alt="Tailwindcss"
-                className="w-10 h-10"
-              />
-            </a>
-            <a href="https://getbootstrap.com/">
-              <img
-                src="/technologies-logo/bootstrap-5.svg"
-                alt="Bootstrap 5"
-                className="w-10 h-10"
-              />
-            </a>
+            {technologiesFrontEnd.map(({ ref, path, text }) => (
+              <a href={ref}>
+                <img
+                  src={`/technologies-logo${path}`}
+                  alt={text}
+                  className="w-10 h-10 mb-2"
+                />
+              </a>
+            ))}
           </div>
         </div>
-        <div className="w-1/2 rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40 text-zinc-90 text-sm font-semibold dark:text-zinc-100">
-          <h4 className="ml-3">Back end</h4>
+        <div className="sm:w-1/2 rounded-2xl border border-zinc-100 p-4 sm:p-5 dark:border-zinc-700/40 text-zinc-900 text-sm font-semibold dark:text-zinc-100">
+          <h4>Back end</h4>
           <div className="flex flex-wrap justify-center mt-4 space-x-2">
-            <a href="https://nodejs.org/en/">
-              <img
-                src="/technologies-logo/nodejs.svg"
-                alt="Nodejs"
-                className="w-10 h-10"
-              />
-            </a>
-            <a href="https://expressjs.com/">
-              <img
-                src="/technologies-logo/expressjs.svg"
-                alt="Express.js"
-                className="w-10 h-10"
-              />
-            </a>
-            <a href="https://www.mysql.com/">
-              <img
-                src="/technologies-logo/mysql.svg"
-                alt="MySQL"
-                className="w-10 h-10"
-              />
-            </a>
-            <a href="https://sequelize.org/">
-              <img
-                src="/technologies-logo/sequelizejs.svg"
-                alt="Sequelize"
-                className="w-10 h-10"
-              />
-            </a>
-            <a href="https://www.typescriptlang.org/">
-              <img
-                src="/technologies-logo/typescript.svg"
-                alt="TypeScript"
-                className="w-10 h-10"
-              />
-            </a>
-            <a href="https://www.mongodb.com/">
-              <img
-                src="/technologies-logo/mongodb.svg"
-                alt="MongoDB"
-                className="w-10 h-10"
-              />
-            </a>
-            <a href="https://mongoosejs.com/">
-              <img
-                src="/technologies-logo/mongoose.svg"
-                alt="Mongoose"
-                className="w-10 h-10"
-              />
-            </a>
+            {technologiesBackEnd.map(({ ref, path, text }) => (
+              <a href={ref}>
+                <img
+                  src={`/technologies-logo${path}`}
+                  alt={text}
+                  className="w-10 h-10 mb-2"
+                />
+              </a>
+            ))}
           </div>
         </div>
       </div>
